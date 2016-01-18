49827b77305c816923bfe85fd670569fc94ec291
Xackery Xtal <xackery@gmail.com>
2016-01-18 01:26:39 -0800

    build tweaks

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 echo Building Linux
 GOOS=linux GOARCH=amd64 go build main.go
-mv main bin/linux/amd64/eqcleanup
+mv main bin/eqcleanup-linux64
 GOOS=linux GOARCH=386 go build main.go
-mv main bin/linux/386/eqcleanup
+mv main bin/eqcleanup-linux386
 echo Building Windows
 GOOS=windows GOARCH=amd64 go build main.go
-mv main.exe bin/windows/amd64/eqcleanup.exe
+mv main.exe bin/eqcleanup-windows64.exe
 GOOS=windows GOARCH=386 go build main.go
-mv main.exe bin/windows/386/eqcleanup.exe
+mv main.exe bin/eqcleanup-windows386.exe
 echo Building OSX
 GOOS=darwin GOARCH=amd64 go build main.go
-mv main bin/darwin/amd64/eqcleanup
+mv main bin/eqcleanup-osx-64
