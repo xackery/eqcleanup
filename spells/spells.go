e832898d5eb691cb915c639508e03bc680362b6a
Xackery Xtal <xackery@gmail.com>
2016-01-23 22:09:29 -0800

    Added some empty spell NPCs when you run Clean on spells

--- spells/spells.go
+++ spells/spells.go
@@ -6,15 +6,42 @@ import (
 	"github.com/jmoiron/sqlx"
 	"github.com/xackery/eqcleanup/eqemuconfig"
 	"github.com/xackery/eqcleanup/item"
+	"github.com/xackery/eqcleanup/spawngroup"
 )
 
 var focus = "spells"
 
 func Clean(db *sqlx.DB, config *eqemuconfig.Config) (err error) {
+
+	//SpawnGroups
+	ids := []int64{
+		//Neriakc
+		//cleric
+		3162, //svlia in Neriakc
+		3163, //Isshia
+		3164, //trik
+		3160, //lyniv
+		3159, //myrish
+		3165, //sol
+		//wizard
+		3142, //jusar
+		3141, //misar
+		5791, //drisi
+		5792, //ash
+		//SK
+		3152,
+		//Necro
+		3154,
+	}
+	totalChanged, err := spawngroup.RemoveSpawnGroupAndEntryById(db, ids)
+	if err != nil {
+		err = fmt.Errorf("Error removing", focus, "entries: %s", err.Error())
+		return
+	}
+
 	//Find all item IDS
 
 	fmt.Println("This script takes a while...")
-	ids := []int64{}
 	rows, err := db.Query("SELECT id FROM items WHERE scrolleffect > 0")
 	if err != nil {
 		return
