4996a5dbabecf773fb8c6dc46070d81c698e42bb
Xackery Xtal <xackery@gmail.com>
2016-03-16 17:02:22 -0700

    Added versioned build file

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
+export VERSION="0.12"
+export NAME="eqcleanup"
 echo Building Linux
 GOOS=linux GOARCH=amd64 go build main.go
-mv main bin/eqcleanup-linux64
+mv main bin/$NAME-$VERSION-linux-x64
 GOOS=linux GOARCH=386 go build main.go
-mv main bin/eqcleanup-linux386
+mv main bin/$NAME-$VERSION-linux-x86
 echo Building Windows
 GOOS=windows GOARCH=amd64 go build main.go
-mv main.exe bin/eqcleanup-windows64.exe
+mv main.exe bin/$NAME-$VERSION-windows-x64.exe
 GOOS=windows GOARCH=386 go build main.go
-mv main.exe bin/eqcleanup-windows386.exe
+mv main.exe bin/$NAME-$VERSION-windows-x86.exe
 echo Building OSX
 GOOS=darwin GOARCH=amd64 go build main.go
-mv main bin/eqcleanup-osx-64
+mv main bin/$NAME-$VERSION-osx-x64
