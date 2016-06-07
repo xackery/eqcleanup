36ae01a6e92eedf3daa2d561fd2cee95a32c71e6
Xackery Xtal <xackery@gmail.com>
2016-06-06 17:27:53 -0700

    added glowing * items and some other.

--- era/era.go
+++ era/era.go
@@ -1,6 +1,7 @@
 package era
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	"github.com/xackery/eqcleanup/item"
@@ -15,12 +16,37 @@ func Clean(db *sqlx.DB, config *eqemuconfig.Config) (err error) {
 	//Mobs
 	ids := []int64{
 		111050, //Gellrazz Scalerunner
+		25429,  //a dark spirit of nektulos
+		59438,  //coldwind blackfoot
+		48343,  //immug lashtail
+		13138,  //frog invasion
+
 	}
 	//#checkpoint_ten
 
 	spawngroup.RemoveSpawnGroupAndEntryById(db, ids)
 
 	//Items
+	ids = loadIds()
+	totalChanged, err := item.RemoveAllInstancesOfItems(db, ids)
+	if err != nil {
+		return
+	}
+	fmt.Println("Removed", totalChanged, " DB entries related to", focus, "in all player-accessible item locations.")
+
+	filePaths := []string{
+	//"befallen/Wraps_McGee.lua",
+	}
+
+	delCount, err := quest.Remove(config, filePaths)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println("Deleted", delCount, focus, "related quest files")
+	return
+}
+
+func loadIds() (ids []int64) {
 	ids = []int64{
 		84186,  //collector's pickaxe
 		69240,  //Collector's Lightstone
@@ -257,21 +283,35 @@ func Clean(db *sqlx.DB, config *eqemuconfig.Config) (err error) {
 		44560,  //consigned bite items
 		56907,  //consigned bite items
 		124669, //consigned bite items
+		97864,  //Raw Fine Runic Hide
+		87563,  //Steamfont Geysers
+		41983,  //Glowing Athlai Hat
+		36286,  //Ornate Binding Powder
+		38691,  //Complex Platinum Silvered Rune
+		35085,  //Curzon
+		87516,  //Santug's Gift
+		79639,  //Silverwing Cloak
+		44764,  //grade c gormar venom
+		41994,  //glowing reis line
+		41995,  //glowing reis
+		41996,  //glowing reis
+		41997,  //glowing reis
+		41998,  //glowing reis
+		41999,  //glowing reis
+		42000,  //glowing reis
+		41987,  //glowing othni
+		41988,  //glowing othni
+		41989,  //glowing othni
+		41990,  //glowing othni
+		41991,  //glowing othni
+		41992,  //glowing othni
+		41993,  //glowing othni
+		41980,  //glowing athlai
+		41981,  //glowing athlai
+		41982,  //glowing athlai
+		41983,  //glowing athlai
+		41984,  //glowing athlai
+		41985,  //glowing athlai
+		41986,  //glowing athlai
 	}
-	totalChanged, err := item.RemoveAllInstancesOfItems(db, ids)
-	if err != nil {
-		return
-	}
-	fmt.Println("Removed", totalChanged, " DB entries related to", focus, "in all player-accessible item locations.")
-
-	filePaths := []string{
-	//"befallen/Wraps_McGee.lua",
-	}
-
-	delCount, err := quest.Remove(config, filePaths)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	fmt.Println("Deleted", delCount, focus, "related quest files")
-	return
 }
