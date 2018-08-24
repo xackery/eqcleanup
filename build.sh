02b209266008656bf2d829ac978f83ae7dba128c
twimjeeni <twimjeeni@users.noreply.github.com>
2018-08-23 20:42:53 -0700

    modify linux build script change main.go to mainmenu.go

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
-export VERSION="0.14"
+export VERSION="0.15"
 export NAME="eqcleanup"
 echo Building Linux $VERSION
-GOOS=linux GOARCH=amd64 go build main.go
+GOOS=linux GOARCH=amd64 go build mainmenu.go
 mv main bin/$NAME-$VERSION-linux-x64
-GOOS=linux GOARCH=386 go build main.go
+GOOS=linux GOARCH=386 go build mainmenu.go
 mv main bin/$NAME-$VERSION-linux-x86
 
 echo Building Windows $VERSION
-GOOS=windows GOARCH=amd64 go build main.go
+GOOS=windows GOARCH=amd64 go build mainmenu.go
 mv main.exe bin/$NAME-$VERSION-windows-x64.exe
-GOOS=windows GOARCH=386 go build main.go
+GOOS=windows GOARCH=386 go build mainmenu.go
 mv main.exe bin/$NAME-$VERSION-windows-x86.exe
 
 echo Building OSX $VERSION
-GOOS=darwin GOARCH=amd64 go build main.go
+GOOS=darwin GOARCH=amd64 go build mainmenu.go
 mv main bin/$NAME-$VERSION-osx-x64
